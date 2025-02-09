24c75f7227993de9fa47574bad72a5857c958ff6
arnav <arnav.bholausa@gmail.com>
2025-02-09 00:23:49 -0500

    fixing deploy error 3

--- app/submitted/page.tsx
+++ app/submitted/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Link from "next/link";
 import HeartBackground from "../../components/HeartBackground";
 
@@ -7,7 +6,7 @@ export default function SubmittedPage({ searchParams }: { searchParams: { name: 
     <main className='min-h-screen flex items-center justify-center bg-pink-100 relative overflow-hidden'>
       <HeartBackground />
       <div className='z-10 bg-white bg-opacity-80 p-8 rounded-lg shadow-lg max-w-md w-full text-center'>
-        <h1 className='text-3xl font-bold text-red-600 mb-6'>Thank You, {searchParams.name}!</h1>
+        <h1 className='text-3xl font-bold text-red-600 mb-6'>Thank You!</h1>
         <p className='text-lg mb-6'>Your Valentine's Day survey has been submitted.</p>
         <Link
           href='/'
